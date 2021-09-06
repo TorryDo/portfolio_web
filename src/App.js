d61ac9e4e698381c6f5f7655a202c24863dcc88d
TorryDo <neemon.teen@gmail.com>
2021-09-06 21:02:12 +0700

    adding main background

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import DashBoardPage from "./components/dashBoardPage/DashBoardPage.js";
 import NavBar from "./components/navBar/NavBar.js";
 import "./App.css";
 
+import mainBackground from './assets/image/earth.jpg';
+
 
 function App() {
   return (
     <div>
+      <div id="mainBackground" alt="background"></div>
       <NavBar></NavBar>
       <DashBoardPage></DashBoardPage>
     </div>
